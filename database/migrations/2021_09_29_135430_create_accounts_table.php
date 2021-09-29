a4d810afbed696f28e5f57d9008c591e21bf306b
Fandresena Randrianjafinjohary <fandresena.randrianjafinjohary@cpp-system.com>
2021-09-29 17:04:33 +0300

    Migration

---
+++ database/migrations/2021_09_29_135430_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->float('balance', 8, 2);
+            $table->boolean('active')->default(0);
+            $table->integer('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'IndexController@index')->name('index')->middleware('auth');
+
+Route::get('login', 'AuthController@login')->name('login');
