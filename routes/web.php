f425966ac128b2950669c47d3515c09bdd53aff6
Fandry Noutah <jfcfandresena@gmail.com>
2021-10-01 08:42:31 +0300

    Login

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'IndexController@index')->name('index')->middleware('auth');
 
-Route::get('login', 'AuthController@login')->name('login');
+Route::get('login', 'LoginController@login')->name('login');
